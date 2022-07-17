c0974c1ccde8e6bae89a4c8e9d688de645362d6c
Arsenii Dunaev <dunaev.arseniy@gmail.com>
2022-07-17 17:51:59 +0200

    Increase linear velocity goal
    Hint: Use with PPO's trial number 23 or above

--- envtest/ros/rl_example.py
+++ envtest/ros/rl_example.py
@@ -38,7 +38,7 @@ def rl_example(state, obstacles, rl_policy=None):
     obs_vec = np.array(obs_vec)
 
     # Convert state to vector observation
-    goal_vel = np.array([3.0, 0.0, 0.0])
+    goal_vel = np.array([5.0, 0.0, 0.0])
 
     att_aray = np.array([state.att[1], state.att[2], state.att[3], state.att[0]])
     rotation_matrix = R.from_quat(att_aray).as_matrix().reshape((9,), order="F")
